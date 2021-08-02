c37dc9fc551885411e6cab2f2298dc91128144a6
Ben Keeney <keene077@umn.edu>
2021-08-02 16:20:43 -0500

    Add ZendeskAPI class

--- src/main/java/zendesk/util/ZendeskAPI.java
+++ src/main/java/zendesk/util/ZendeskAPI.java
@@ -1,4 +1,63 @@
 package zendesk.util;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Base64;
+import java.util.Scanner;
+
 public class ZendeskAPI {
+    private String encryptedAuth;
+    private String hostURL;
+
+    public String makeGetRequest(String request){
+        try {
+            StringBuilder result = new StringBuilder();
+            java.net.URL url = new URL(hostURL + request);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.addRequestProperty("Authorization", "Basic " + encryptedAuth);
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                for (String line; (line = reader.readLine()) != null; ) {
+                    result.append(line);
+                }
+            }
+            return result.toString();
+
+        } catch (MalformedURLException e){
+            System.out.println( "NONSENSE");
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void setCredentials(String configPath){
+        try {
+            Scanner scanner = new Scanner(new File(configPath));
+            String email = "";
+            String token = "";
+            String subdomain = "";
+            while (scanner.hasNext()){
+                String[] line = scanner.nextLine().split("=");
+                switch (line[0]) {
+                    case "email" -> email = line[1];
+                    case "token" -> token = line[1];
+                    case "subdomain" -> subdomain = line[1];
+                }
+            }
+            String rawAuthorization = email + "/token:" + token;
+            encryptedAuth = Base64.getEncoder().encodeToString(rawAuthorization.getBytes());
+            hostURL = "https://" + subdomain + ".zendesk.com";
+        } catch (FileNotFoundException e){
+            System.out.println("config.properties not found!");
+        }
+    }
 }
