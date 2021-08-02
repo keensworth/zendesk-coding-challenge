5cce8d95adc61808f36e22ef576751aa68168882
Ben Keeney <keene077@umn.edu>
2021-08-02 16:20:00 -0500

    Delegate API functionality into separate class

--- src/main/java/zendesk/model/TicketManager.java
+++ src/main/java/zendesk/model/TicketManager.java
@@ -1,4 +1,56 @@
 package zendesk.model;
 
 public class TicketManager {
+    private TicketFetcher ticketFetcher;
+    private Ticket[] currentTicketPage;
+    private Ticket currentTicket;
+    private int pageNum;
+
+    public TicketManager(){
+        pageNum = 1;
+    }
+
+    public Ticket getTicket(int ticketId){
+        Ticket ticket = ticketFetcher.fetchTicket(ticketId);
+        currentTicket = ticket;
+        return ticket;
+    }
+
+    public Ticket[] viewAllTickets(){
+        Ticket[] tickets = ticketFetcher.fetchInitialTicketPage();
+        currentTicketPage = tickets;
+        return tickets;
+    }
+
+    public Ticket[] getNextPage(){
+        if (ticketFetcher.hasMoreTickets()) {
+            Ticket[] tickets = ticketFetcher.fetchNextTicketPage();
+            currentTicketPage = tickets;
+            pageNum++;
+            return tickets;
+        }
+        return null;
+    }
+
+    public Ticket[] getPrevPage(){
+        if (pageNum > 1){
+            Ticket[] tickets = ticketFetcher.fetchPrevTicketPage();
+            currentTicketPage = tickets;
+            pageNum--;
+            return tickets;
+        }
+        return null;
+    }
+
+    public Ticket[] getCurrentTicketPage(){
+        return currentTicketPage;
+    }
+
+    public Ticket getCurrentTicket(){
+        return currentTicket;
+    }
+
+    public int getPageNum(){
+        return pageNum;
+    }
 }
