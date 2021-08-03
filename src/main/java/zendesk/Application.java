b93819c9b8d3ed41c8e3d679d7e43bc02aec5735
Ben Keeney <keene077@umn.edu>
2021-08-03 03:17:03 -0500

    Optimize imports

--- src/main/java/zendesk/Application.java
+++ src/main/java/zendesk/Application.java
@@ -1,4 +1,31 @@
 package zendesk;
 
+import zendesk.model.ConsoleManager;
+import zendesk.util.ZendeskAPI;
+
 public class Application {
+    private final ConsoleManager consoleManager;
+
+
+    public Application(){
+        ZendeskAPI zendeskAPI = new ZendeskAPI();
+        zendeskAPI.setCredentials("src/main/resources/config.properties");
+
+        consoleManager = new ConsoleManager(zendeskAPI);
+    }
+
+
+    public void run(){
+        int state = -1;
+
+        while (state != ConsoleManager.QUIT_STATE){
+            state = consoleManager.assessState();
+        }
+    }
+
+
+    public static void main(String[] args){
+        Application app = new Application();
+        app.run();
+    }
 }
